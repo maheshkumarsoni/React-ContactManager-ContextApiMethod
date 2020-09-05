a1918a326db72725c66260e439ad2ad480fc5b87
maheshkumarsoni <sms3299sms@gmail.com>
2020-09-05 09:23:16 +0530

    Updates

---
+++ precache-manifest.6f4aefb0a4d35fe29fb2b6766ee6c66d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb550de24e3b8eea6ee6d77e712e9db",
+    "url": "/React-ContactManager-ContextApiMethod/index.html"
+  },
+  {
+    "revision": "ebffe4bb5ccbaad570f6",
+    "url": "/React-ContactManager-ContextApiMethod/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ebffe4bb5ccbaad570f6",
+    "url": "/React-ContactManager-ContextApiMethod/static/js/2.52a9a28f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-ContactManager-ContextApiMethod/static/js/2.52a9a28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8255c78bcfc4e58e41be",
+    "url": "/React-ContactManager-ContextApiMethod/static/js/main.49117369.chunk.js"
+  },
+  {
+    "revision": "61e25f288ba10273b79d",
+    "url": "/React-ContactManager-ContextApiMethod/static/js/runtime-main.6bff6247.js"
+  }
+]);
